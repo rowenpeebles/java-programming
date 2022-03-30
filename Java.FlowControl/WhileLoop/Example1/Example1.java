3a4d1a6c8941b830e330a418604fe495c6b9049d
Rowen Peebles <rowen.peebles@outlook.com>
2022-03-30 13:36:48 +0100

    Adding Java While

---
+++ Java.FlowControl/WhileLoop/Example1/Example1.java
@@ -0,0 +1,16 @@
+// Display Numbers from 1 to 5
+
+class Example1 {
+
+    public static void main(String[] args) {
+
+        int i = 1, n = 5;
+
+        while (i <= n) {
+            System.out.println(i);
+            ++i;
+        }
+
+    }
+
+}
---
+++ Java.FlowControl/WhileLoop/Example2/Example2.java
@@ -0,0 +1,27 @@
+// Sum of Positive Numbers Only
+
+import java.util.Scanner;
+
+class Example2 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int number = input.nextInt();
+
+        while (number >= 0) {
+            sum += number;
+            System.out.println("Enter a number");
+            number = input.nextInt();
+        }
+
+        System.out.println("Sum = " + sum);
+        input.close();
+
+    }
+
+}
