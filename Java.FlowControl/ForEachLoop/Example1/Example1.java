1070daa63c609361618437c79c5033cc076037ee
Rowen Peebles <rowen.peebles@outlook.com>
2022-03-30 11:42:07 +0100

    Adding Java Foreach

---
+++ Java.FlowControl/ForEachLoop/Example1/Example1.java
@@ -0,0 +1,15 @@
+// Print Array Elements
+
+class Example1 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 9, 5, -5};
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+    }
+
+}
