3958990516857610ac935ba6201c487cdf89c7f0
Rowen Peebles <rowen.peebles@outlook.com>
2022-04-07 18:09:28 +0100

    Adding Java OOP1 Recursion

---
+++ Java.OOP1/Recursion/Example.java
@@ -0,0 +1,25 @@
+// Factorial of a Number Using Recursion
+
+class Example {
+
+    static int factorial(int n) {
+
+        if (n != 0) {
+            return n * factorial(n-1);
+        }
+
+        else {
+            return 1;
+        }
+    
+    }
+    
+    public static void main(String[] args) {
+
+        int number = 4, result;
+        result = factorial(number);
+        System.out.println(number + " factorial = " + result);
+
+    }
+
+}
