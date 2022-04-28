60bb84a09ae3a7ba735e2ba25a8516bda16819da
Rowen Peebles <rowen.peebles@outlook.com>
2022-04-28 14:31:34 +0100

    Adding Java Assertions

---
+++ Java.ExceptionHandling/Assertions/Example1/Example1.java
@@ -0,0 +1,11 @@
+// Java Assertion
+
+class Example1 {
+    public static void main(String[] args) {
+        
+        String[] weekends = {"Friday", "Saturday", "Sunday"};
+        assert weekends.length == 2;
+        System.out.println("There are " + weekends.length + " weekends in a week");
+    
+    }
+}
---
+++ Java.ExceptionHandling/Assertions/Example2/Example2.java
@@ -0,0 +1,12 @@
+// Java assertion with expression example
+
+class Example2 {
+
+    public static void main(String[] args) {
+
+        String[] weekends = {"Friday", "Saturday", "Sunday"};
+        assert weekends.length == 2 : "There are only 2 weekends in a week";
+        System.out.println("There are " + weekends.length + " weekends in a week");
+
+    }
+}
