d98ea2b5a9641160fe96f58abbde55912900e1bc
Rowen Peebles <rowen.peebles@outlook.com>
2022-04-28 16:24:53 +0100

    Adding Java CollectionsFramework

---
+++ Java.List/CollectionsFramework/Example.java
@@ -0,0 +1,19 @@
+// ArrayList Class of Collections
+
+import java.util.ArrayList;
+
+class Example {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> animals = new ArrayList<>();
+
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.println("ArrayList: " + animals);
+
+    }
+
+}
