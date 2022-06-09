b626b5b79a3fb7bc5a9d3019a14818bbd7661486
Rowen Peebles <rowen.peebles@outlook.com>
2022-06-09 13:13:16 +0100

    Adding Java TypeCasting

---
+++ Java.AdditionalTopics/TypeCasting/Example1/Example1.java
@@ -0,0 +1,13 @@
+// Type conversion from Int to String
+
+class Example1 {
+    public static void main(String[] args) {
+
+        int num = 10;
+        System.out.println("The integer value is: " + num);
+        
+        // Converts int type to string type
+        String data = String.valueOf(num);
+        System.out.println("The string value is: " + data);
+    }
+}
---
+++ Java.AdditionalTopics/TypeCasting/Example2/Example2.java
@@ -0,0 +1,13 @@
+// Type conversion from String type to Int type
+
+class Example2 {
+    public static void main(String[] args) {
+
+        String data = "10";
+        System.out.println("The string value is: " + data);
+
+        // Convert string to int
+        int num = Integer.parseInt(data);
+        System.out.println("The integer value is: " + num);
+    }
+}
